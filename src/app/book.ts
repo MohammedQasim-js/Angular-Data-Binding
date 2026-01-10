a5b265db2477f9e0c659273441824a124c4b0f8b
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2026-01-10 18:40:12 +0530

    Form Validation - Template driven

--- src/app/book.ts
+++ src/app/book.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { BookRepository } from './repository.model';
 import { Book } from './book.model';
 import { CustomPipe } from './custom.pipe';
@@ -79,14 +79,74 @@ import { ForEmail } from './for-email';
 // }
 export class BookComponent {
   model: BookRepository = new BookRepository();
-  bookName: string | undefined = this.model.getBooksId(1)?.name;
-  addBook() {
-    this.model.addBook(new Book(4, 'Omniscient Reader Viewpoint', 'singNsong', 399));
+
+  newBook: Book = new Book();
+  get jsonBook() {
+    return JSON.stringify(this.newBook);
   }
-  deleteBook(book: Book) {
-    this.model.deleteBook(book);
+
+  addBook(b: Book) {
+    console.log('New Book is ' + this.jsonBook);
   }
-  updateBook(book: Book) {
-    book.name = 'Updated';
+
+  getFormValitdationErrors(form: NgForm): string[] {
+    let messages: string[] = [];
+
+    Object.keys(form.controls).forEach((x) => {
+      console.log(x);
+      console.log(form.controls[x]);
+
+      this.getValidationErrors(form.controls[x], x).forEach((message) => messages.push(message));
+    });
+
+    return messages;
   }
+
+  getValidationErrors(model: any, key: string) {
+    let ctrlName: string = model.name || key;
+    let messages: string[] = [];
+
+    if (model.errors) {
+      for (let errorName in model.errors) {
+        switch (errorName) {
+          case 'required':
+            messages.push(`You must enter a ${ctrlName}.`);
+            break;
+          case 'minlength':
+            messages.push('You must enter at least 3 characters.');
+            break;
+        }
+      }
+    }
+    return messages;
+  }
+
+  formSubmit: boolean = false;
+
+  submitForm(form: NgForm) {
+    // console.log(form);
+    this.formSubmit = true;
+    if (form.valid) {
+      this.addBook(this.newBook);
+      this.newBook = new Book();
+      form.reset();
+      this.formSubmit = false;
+    }
+  }
+
+  // log(model: any) {
+  //   console.log(model);
+  // }
+
+  // For Directive section
+  // bookName: string | undefined = this.model.getBooksId(1)?.name;
+  // addBook() {
+  //   this.model.addBook(new Book(4, 'Omniscient Reader Viewpoint', 'singNsong', 399));
+  // }
+  // deleteBook(book: Book) {
+  //   this.model.deleteBook(book);
+  // }
+  // updateBook(book: Book) {
+  //   book.name = 'Updated';
+  // }
 }
