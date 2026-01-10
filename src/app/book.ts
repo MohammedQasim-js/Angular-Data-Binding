71c302a4a6f0854ef9ffdcd1a4c015214d520f5a
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2026-01-10 20:15:44 +0530

    Form Validation - Reactive Form

--- src/app/book.ts
+++ src/app/book.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, NgForm } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  NgForm,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { BookRepository } from './repository.model';
 import { Book } from './book.model';
 import { CustomPipe } from './custom.pipe';
 import { ForEmail } from './for-email';
+import { ImageValidator } from './image.validator';
 
 @Component({
   selector: 'app-book',
-  imports: [CommonModule, FormsModule, CustomPipe, ForEmail],
+  imports: [CommonModule, FormsModule, CustomPipe, ForEmail, ReactiveFormsModule],
   templateUrl: './book.html',
   styleUrls: ['./book.css'],
 })
@@ -77,76 +85,118 @@ import { ForEmail } from './for-email';
 //     console.log(this.name);
 //   }
 // }
+
+// Form Controls
+// export class BookComponent {
+//   model: BookRepository = new BookRepository();
+
+//   newBook: Book = new Book();
+//   get jsonBook() {
+//     return JSON.stringify(this.newBook);
+//   }
+
+//   addBook(b: Book) {
+//     console.log('New Book is ' + this.jsonBook);
+//   }
+
+//   getFormValitdationErrors(form: NgForm): string[] {
+//     let messages: string[] = [];
+
+//     Object.keys(form.controls).forEach((x) => {
+//       console.log(x);
+//       console.log(form.controls[x]);
+
+//       this.getValidationErrors(form.controls[x], x).forEach((message) => messages.push(message));
+//     });
+
+//     return messages;
+//   }
+
+//   getValidationErrors(model: any, key: string) {
+//     let ctrlName: string = model.name || key;
+//     let messages: string[] = [];
+
+//     if (model.errors) {
+//       for (let errorName in model.errors) {
+//         switch (errorName) {
+//           case 'required':
+//             messages.push(`You must enter a ${ctrlName}.`);
+//             break;
+//           case 'minlength':
+//             messages.push('You must enter at least 3 characters.');
+//             break;
+//         }
+//       }
+//     }
+//     return messages;
+//   }
+
+//   formSubmit: boolean = false;
+
+//   submitForm(form: NgForm) {
+//     // console.log(form);
+//     this.formSubmit = true;
+//     if (form.valid) {
+//       this.addBook(this.newBook);
+//       this.newBook = new Book();
+//       form.reset();
+//       this.formSubmit = false;
+//     }
+//   }
+
+//   // log(model: any) {
+//   //   console.log(model);
+//   // }
+
+//   // For Directive section
+//   // bookName: string | undefined = this.model.getBooksId(1)?.name;
+//   // addBook() {
+//   //   this.model.addBook(new Book(4, 'Omniscient Reader Viewpoint', 'singNsong', 399));
+//   // }
+//   // deleteBook(book: Book) {
+//   //   this.model.deleteBook(book);
+//   // }
+//   // updateBook(book: Book) {
+//   //   book.name = 'Updated';
+//   // }
+// }
+
+// Reactive Forms
 export class BookComponent {
-  model: BookRepository = new BookRepository();
+  bookForm = new FormGroup({
+    // name: new FormControl('Dr. Stone'),
+    // writer: new FormControl('Riichiro Inagaki'),
+    // price: new FormControl('299'),
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    writer: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required),
+    image: new FormControl('', [Validators.required, ImageValidator.isValidxtension]),
+  });
 
-  newBook: Book = new Book();
-  get jsonBook() {
-    return JSON.stringify(this.newBook);
+  get name() {
+    return this.bookForm.get('name');
   }
 
-  addBook(b: Book) {
-    console.log('New Book is ' + this.jsonBook);
+  get image() {
+    return this.bookForm.get('image');
   }
 
-  getFormValitdationErrors(form: NgForm): string[] {
-    let messages: string[] = [];
+  onSubmit() {
+    console.log(this.bookForm.value);
+  }
 
-    Object.keys(form.controls).forEach((x) => {
-      console.log(x);
-      console.log(form.controls[x]);
-
-      this.getValidationErrors(form.controls[x], x).forEach((message) => messages.push(message));
+  updateBook() {
+    this.bookForm.patchValue({
+      name: 'Attack on Titan',
+      price: '399',
     });
-
-    return messages;
   }
 
-  getValidationErrors(model: any, key: string) {
-    let ctrlName: string = model.name || key;
-    let messages: string[] = [];
+  // name = new FormControl('Dr. Stone');
+  // writer = new FormControl('Riichiro Inagaki');
+  // price = new FormControl('299');
 
-    if (model.errors) {
-      for (let errorName in model.errors) {
-        switch (errorName) {
-          case 'required':
-            messages.push(`You must enter a ${ctrlName}.`);
-            break;
-          case 'minlength':
-            messages.push('You must enter at least 3 characters.');
-            break;
-        }
-      }
-    }
-    return messages;
-  }
-
-  formSubmit: boolean = false;
-
-  submitForm(form: NgForm) {
-    // console.log(form);
-    this.formSubmit = true;
-    if (form.valid) {
-      this.addBook(this.newBook);
-      this.newBook = new Book();
-      form.reset();
-      this.formSubmit = false;
-    }
-  }
-
-  // log(model: any) {
-  //   console.log(model);
-  // }
-
-  // For Directive section
-  // bookName: string | undefined = this.model.getBooksId(1)?.name;
-  // addBook() {
-  //   this.model.addBook(new Book(4, 'Omniscient Reader Viewpoint', 'singNsong', 399));
-  // }
-  // deleteBook(book: Book) {
-  //   this.model.deleteBook(book);
-  // }
-  // updateBook(book: Book) {
-  //   book.name = 'Updated';
+  // updateName() {
+  //   this.name.setValue('Demon Slayer');
   // }
 }
